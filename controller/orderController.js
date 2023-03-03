f47ef203b10a165bc8e85b7fed025128ebbc913d
DaphneGoh89 <115914140+DaphneGoh89@users.noreply.github.com>
2023-03-04 01:31:28 +0800

    Create Order API completed

--- controller/orderController.js
+++ controller/orderController.js
@@ -3,14 +3,64 @@ const { expand, flatten } = require("../helpers/helperFunction");
 
 const createOrder = async (req, res) => {
   try {
-    const { firstName, lastName, orderDetails } = req.body;
+    // Destructure request body
+    const {
+      customerId,
+      firstName,
+      lastName,
+      orderCurrency,
+      orderTotal,
+      giftcard,
+      promocode,
+      giftcardValue,
+      promocodeValue,
+      shippingFee,
+      gstAmount,
+      shiptoCountry,
+      shiptoAddrline1,
+      shiptoAddrline2,
+      shiptoPostal,
+      contactCtrycode,
+      contactNumber,
+      deliveryMethod,
+      deliveryTiming,
+      paymentMethod,
+      paymentAmount,
+      orderStatus,
+      orderDetails,
+    } = req.body;
 
+    // Begin insert queries
     await pool.query("BEGIN");
 
-    // Insert into "order_headers" table
-    const insertOrder =
-      "INSERT INTO order_headers (first_name, last_name) VALUES($1, $2) RETURNING id";
-    const response = await pool.query(insertOrder, [firstName, lastName]);
+    // Insert into "order" table
+    const insertOrder = `INSERT INTO "order" ("customer_id", "first_name", "last_name", "order_currency", "order_total", "giftcard", "promocode", 
+        "giftcard_value", "promocode_value", "shipping_fee", "gst_amount", "shipto_country", "shipto_addrline1", "shipto_addrline2",
+        "shipto_postal", "contact_ctrycode", "contact_number", "delivery_method", "delivery_timing", "payment_method", "payment_amount") 
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21) RETURNING id`;
+    const response = await pool.query(insertOrder, [
+      customerId,
+      firstName,
+      lastName,
+      orderCurrency,
+      orderTotal,
+      giftcard,
+      promocode,
+      giftcardValue,
+      promocodeValue,
+      shippingFee,
+      gstAmount,
+      shiptoCountry,
+      shiptoAddrline1,
+      shiptoAddrline2,
+      shiptoPostal,
+      contactCtrycode,
+      contactNumber,
+      deliveryMethod,
+      deliveryTiming,
+      paymentMethod,
+      paymentAmount,
+    ]);
 
     // Insert "order_id" and "line_id" into "orderDetails" from JSON body
     const orderLineParams = orderDetails.map((item, index) => [
@@ -19,10 +69,10 @@ const createOrder = async (req, res) => {
       index,
     ]);
 
-    // Insert into "order_lines" table
-    const insertOrderlines = `INSERT INTO order_lines (product_code, product_name, quantity, order_id, line_id) VALUES ${expand(
+    // Insert into "order_line" table
+    const insertOrderlines = `INSERT INTO "order_line" (product_code, product_name, product_color, product_size, order_qty, unit_price, line_total, order_id, line_num) VALUES ${expand(
       orderDetails.length,
-      5
+      9
     )}`;
     const orderLineValues = flatten(orderLineParams);
     await pool.query(insertOrderlines, orderLineValues);
